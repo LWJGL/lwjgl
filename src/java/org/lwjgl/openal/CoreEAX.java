9ecf595bd401d22caf9c02ec3efe13a501789607
Brian Matzon <brian@matzon.dk>
2002-08-30 21:30:40 +0000

    add: first stab at EAX support

---
+++ src/java/org/lwjgl/openal/CoreEAX.java
@@ -0,0 +1,83 @@
+/* 
+ * Copyright (c) 2002 Light Weight Java Game Library Project
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ * 
+ * * Redistributions of source code must retain the above copyright 
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'Light Weight Java Game Library' nor the names of 
+ *   its contributors may be used to endorse or promote products derived 
+ *   from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ package org.lwjgl.openal;
+
+/**
+ * $Id$
+ *
+ * This is the OpenAL EAX class. It extends the latest core.
+ *
+ * @author Brian Matzon <brian@matzon.dk>
+ * @version $Revision$
+ */
+public class CoreEAX extends BaseEAX implements BaseEAXConstants {  
+    
+    /** Creates a new instance of CoreEAX */
+    public CoreEAX() {
+    }
+    
+    /**
+     * Load extensions
+     */
+    protected void init() {        
+		determineAvailableExtensions();
+    }
+    
+    /**
+     * Determines available EAX extensions
+     */
+    protected native void determineAvailableExtensions();
+    
+    /**
+     * Retrieves an EAX Value
+     *
+     * @param propertySetID adress to the property set GUID of the object being queried (a listener or a source)
+     * @param property property being queried
+     * @param source the source to be queried
+     * @param data address to write value to
+     * @param size size of area being written to
+     * @return OpenAL Error code
+     */
+    public native int eaxGet(int propertySetID, int property, int source, int data, int size);
+    
+    /**
+     * Sets an EAX Value
+     *
+     * @param propertySetID adress to the property set GUID of the object being queried (a listener or a source)
+     * @param property property being queried
+     * @param source the source to be queried
+     * @param data address to write value to
+     * @param size size of area being written to
+     * @return OpenAL Error code
+     */
+    public native int eaxSet(int propertySetID, int property, int source, int data, int size);
+}
---
+++ src/native/win32/org_lwjgl_opengl_CoreEAX.cpp
@@ -0,0 +1,141 @@
+/* 
+ * Copyright (c) 2002 Light Weight Java Game Library Project
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ * 
+ * * Redistributions of source code must retain the above copyright 
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'Light Weight Java Game Library' nor the names of 
+ *   its contributors may be used to endorse or promote products derived 
+ *   from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * $Id$
+ *
+ * This is the actual JNI implementation of the OpenAL EAX library. 
+ * 
+ * @author Brian Matzon <brian@matzon.dk>
+ * @version $Revision$
+ */
+#include "org_lwjgl_openal_CoreEAX.h"
+#include "checkALerror.h"
+
+#include "extal.h"
+
+/**
+ * Throws an OAL exception with the specified message
+ */
+void ThrowException(JNIEnv *env, const char* message) {
+	jclass cls = env->FindClass("org/lwjgl/openal/OpenALException");
+	env->ThrowNew(cls, message);
+}
+
+/*
+ * Determines available EAX extensions
+ */
+JNIEXPORT void JNICALL Java_org_lwjgl_openal_CoreEAX_determineAvailableExtensions (JNIEnv *env, jobject obj) {
+#ifdef _WIN32    
+	bool EAXSupported = false;
+	ALCcontext *Context;
+	ALCdevice *Device;
+
+	//open device
+ 	Device = alcOpenDevice(NULL);
+	if(Device == NULL) {
+		ThrowException(env, "Unable to determine EAX Extensions");
+		return;
+	}
+
+	//create a context
+	Context = alcCreateContext(Device, NULL);
+	if(Context == NULL) {
+		alcCloseDevice(Device);
+		ThrowException(env, "Unable to determine EAX Extensions");
+		return;
+	}
+
+	//make current
+	alcMakeContextCurrent(Context);
+	if(alGetError() != AL_NO_ERROR) {
+		alcCloseDevice(Device);
+		ThrowException(env, "Unable to determine EAX Extensions");
+		return;
+	}
+
+	//check for extension, and assign if available
+	if(alIsExtensionPresent((ALubyte*) "EAX")) {
+		eaxSet = (EAXSet)alGetProcAddress((ALubyte*) "EAXSet");
+		eaxGet = (EAXGet)alGetProcAddress((ALubyte*) "EAXGet");
+		EAXSupported = (eaxSet != NULL && eaxGet != NULL);
+	}
+
+	//clean up
+	alcMakeContextCurrent(NULL);
+	alcDestroyContext(Context);
+	alcCloseDevice(Device);
+
+	//throw exception if no EAX support
+	if(EAXSupported != true) {
+		ThrowException(env, "Unable to determine EAX Extensions");
+	}
+#else
+	ThrowException(env, "EAX extensions not supported");
+#endif
+}
+
+/*
+ * This function retrieves an EAX value.
+ * 
+ * C Specification:
+ * ALenum EAXGet(const struct _GUID *propertySetID,ALuint property,ALuint source,ALvoid
+ * *value,ALuint size);
+ */
+JNIEXPORT jint JNICALL Java_org_lwjgl_openal_CoreEAX_eaxGet (JNIEnv *env, jobject obj, jint propertySetID, jint property, jint source, jint value, jint size) {
+#ifdef _WIN32
+	jint result = (jint) eaxGet((const struct _GUID*)propertySetID, (ALuint) property, (ALuint) source, (ALvoid*) value, (ALuint) size);
+	CHECK_AL_ERROR
+	
+	return result;
+#else
+	return AL_INVALID_OPERATION;
+#endif
+}
+
+/*
+ * This function sets an EAX value.
+ * 
+ * C Specification:
+ * ALenum EAXGet(const struct _GUID *propertySetID,ALuint property,ALuint source,ALvoid
+ * *value,ALuint size);
+ */
+JNIEXPORT jint JNICALL Java_org_lwjgl_openal_CoreEAX_eaxSet (JNIEnv *env, jobject obj, jint propertySetID, jint property, jint source, jint value, jint size) {
+#ifndef _WIN32
+	jint result = (jint) eaxSet((const struct _GUID*)propertySetID, (ALuint) property, (ALuint) source, (ALvoid*) value, (ALuint) size);
+	CHECK_AL_ERROR
+	
+	return result;
+#else
+	return AL_INVALID_OPERATION;
+#endif
+}
