bba7bf9462b6c9e2bbdd3dc35e91b89f56b76933
Elias Naur <elias.naur@gmail.com>
2003-03-21 17:37:58 +0000

    Added getPlatform() to linux

--- src/native/linux/org_lwjgl_Display.cpp
+++ src/native/linux/org_lwjgl_Display.cpp
@@ -508,3 +508,13 @@ JNIEXPORT jobjectArray JNICALL Java_org_lwjgl_Display_nGetAvailableDisplayModes
 	return ret;
 }
 
+/*
+ * Class:     org_lwjgl_Display
+ * Method:    getPlatform
+ * Signature: ()I
+ */
+JNIEXPORT jint JNICALL Java_org_lwjgl_Display_getPlatform
+  (JNIEnv * env, jclass clazz)
+{
+	return org_lwjgl_Display_PLATFORM_GLX;
+}
