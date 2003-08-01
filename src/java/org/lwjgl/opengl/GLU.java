1ddaaf773e68325eab1988fb10836c1faf5c3ca2
Caspian Rychlik-Prince <cprince@puppygames.net>
2003-08-01 17:53:44 +0000

    Major change to use overloaded buffer functions and buffer positions. In addition, quadrics and default shapes have been removed from GLU.

--- src/java/org/lwjgl/opengl/GLU.java
+++ src/java/org/lwjgl/opengl/GLU.java
@@ -93,7 +93,7 @@ public class GLU implements GLUConstants {
 		double upy,
 		double upz);
 
-	public static native int gluProject(
+	public static int gluProject(
 		double objx,
 		double objy,
 		double objz,
@@ -101,9 +101,25 @@ public class GLU implements GLUConstants {
 		DoubleBuffer projMatrix,
 		IntBuffer viewport,
 		DoubleBuffer win
+	) {
+		return ngluProject(objx, objy, objz, modelMatrix, modelMatrix.position(), projMatrix, projMatrix.position(), viewport, viewport.position(), win, win.position());
+	}
+
+	private static native int ngluProject(
+		double objx,
+		double objy,
+		double objz,
+		DoubleBuffer modelMatrix,
+		int modelMatrix_offset,
+		DoubleBuffer projMatrix,
+		int projMatrix_offset,
+		IntBuffer viewport,
+		int viewport_ofset,
+		DoubleBuffer win,
+		int win_offset
 	);
 
-	public static native int gluUnProject(
+	public static int gluUnProject(
 		double winx,
 		double winy,
 		double winz,
@@ -111,27 +127,67 @@ public class GLU implements GLUConstants {
 		DoubleBuffer projMatrix,
 		IntBuffer viewport,
 		DoubleBuffer obj
+	) {
+		return ngluUnProject(winx, winy, winz, modelMatrix, modelMatrix.position(), projMatrix, projMatrix.position(), viewport, viewport.position(), obj, obj.position());
+	}
+
+	private static native int ngluUnProject(
+		double winx,
+		double winy,
+		double winz,
+		DoubleBuffer modelMatrix,
+		int modelMatrix_offset,
+		DoubleBuffer projMatrix,
+		int projMatrix_offset,
+		IntBuffer viewport,
+		int viewport_offset,
+		DoubleBuffer obj,
+		int obj_offset
 	);
 
-	public static native int gluScaleImage(
+	public static int gluScaleImage(
 		int format,
 		int widthin,
 		int heightin,
 		int typein,
 		ByteBuffer datain,
 		int widthout, int heightout, int typeout, ByteBuffer dataout
+	) {
+		return ngluScaleImage(format, widthin, heightin, typein, datain, datain.position(), widthout, heightout, typeout, dataout, dataout.position());
+	}
+
+	private static native int ngluScaleImage(
+		int format,
+		int widthin,
+		int heightin,
+		int typein,
+		ByteBuffer datain,
+		int datain_offset,
+		int widthout, int heightout, int typeout, ByteBuffer dataout, int dataout_offset
 	);
 
-	public static native int gluBuild1DMipmaps(
+	public static int gluBuild1DMipmaps(
 		int target,
 		int components,
 		int width,
 		int format,
 		int type,
 		ByteBuffer data
+	) {
+		return ngluBuild1DMipmaps(target, components, width, format, type, data, data.position());
+	}
+
+	private static native int ngluBuild1DMipmaps(
+		int target,
+		int components,
+		int width,
+		int format,
+		int type,
+		ByteBuffer data,
+		int data_offset
 	);
 
-	public static native int gluBuild2DMipmaps(
+	public static int gluBuild2DMipmaps(
 		int target,
 		int components,
 		int width,
@@ -139,228 +195,19 @@ public class GLU implements GLUConstants {
 		int format,
 		int type,
 		ByteBuffer data
+	) {
+		return ngluBuild2DMipmaps(target, components, width, height, format, type, data, data.position());
+	}
+
+	private static native int ngluBuild2DMipmaps(
+		int target,
+		int components,
+		int width,
+		int height,
+		int format,
+		int type,
+		ByteBuffer data,
+		int data_offset
 	);
-        
-        /**
-         * creates and returns a pointer to a new quadrics object. This
-         * object must be referred to when calling quadrics rendering and control
-         * functions.  A return value of null means that there is not enough memory to
-         * allocate the object
-         *
-         * @return ByteBuffer representing a new quadrics object
-         */
-        public static native ByteBuffer gluNewQuadric();
-        
-        /**
-         * draws a cylinder oriented along the z axis. The base of the
-         * cylinder is placed at z = 0, and the top at z=height. Like a sphere, a
-         * cylinder is subdivided around the z axis into slices, and along the z axis
-         * into stacks.
-         *
-         * Note that if topRadius is set to zero, then this routine will generate a
-         * cone.
-         *
-         * If the orientation is set to GLU.OUTSIDE (with glu.quadricOrientation), then
-         * any generated normals point away from the z axis. Otherwise, they point
-         * toward the z axis.
-         *
-         * If texturing is turned on (with glu.quadricTexture), then texture
-         * coordinates are generated so that t ranges linearly from 0.0 at z = 0 to
-         * 1.0 at z = height, and s ranges from 0.0 at the +y axis, to 0.25 at the +x
-         * axis, to 0.5 at the -y axis, to 0.75 at the -x axis, and back to 1.0 at the
-         * +y axis.
-         *
-         * @param target  Specifies the quadrics object (created with glu.newQuadric).
-         *
-         * @param baseRadius  Specifies the radius of the cylinder at z = 0.
-         *
-         * @param topRadius   Specifies the radius of the cylinder at z = height.
-         *
-         * @param height      Specifies the height of the cylinder.
-         *
-         * @param slices      Specifies the number of subdivisions around the z axis.
-         *
-         * @param stacks      Specifies the number of subdivisions along the z axis.
-         */
-        public static native void gluCylinder(
-                ByteBuffer target,
-                double baseRadius,
-                double topRadius,
-                double height,
-                int slices,
-                int stacks
-        );
-        
-        /**
-         * destroys the quadrics object and frees any memory used by
-         * it.  Once glu.deleteQuadric has been called, the object cannot be used again.
-         *
-         * @param qobj pecifies the quadrics object to be destroyed (created with
-	 * glu.newQuadric).
-         */
-        public static native void gluDeleteQuadric(
-                ByteBuffer qobj
-        );
-        
-        
-        /**
-         * renders a disk on the z = 0  plane.  The disk has a radius of
-         * outerRadius, and contains a concentric circular hole with a radius of
-         * innerRadius. If innerRadius is 0, then no hole is generated. The disk is
-         * subdivided around the z axis into slices (like pizza slices), and also
-         * about the z axis into rings (as specified by slices and loops,
-         * respectively).
-         *
-         * With respect to orientation, the +z side of the disk is considered to be
-         * "outside" (see glu.quadricOrientation).  This means that if the orientation
-         * is set to GLU.OUTSIDE, then any normals generated point along the +z axis.
-         * Otherwise, they point along the -z axis.
-         *
-         * If texturing is turned on (with glu.quadricTexture), texture coordinates are
-         * generated linearly such that where r=outerRadius, the value at (r, 0, 0) is
-         * (1, 0.5), at (0, r, 0) it is (0.5, 1), at (-r, 0, 0) it is (0, 0.5), and at
-         * (0, -r, 0) it is (0.5, 0).
-         */
-        public static native void gluDisk(
-                ByteBuffer target,
-                double innerRadius,
-                double outerRadius,
-                int slices,
-                int loops
-        );
-
-        /**
-         * renders a partial disk on the z=0 plane.  A partial disk is
-         * similar to a full disk, except that only the subset of the disk from
-         * startAngle through startAngle + sweepAngle is included (where 0 degrees is
-         * along the +y axis, 90 degrees along the +x axis, 180 along the -y axis, and
-         * 270 along the -x axis).
-         *
-         * The partial disk has a radius	of outerRadius,	and contains a concentric
-         * circular hole with a radius of innerRadius.  If innerRadius is zero, then
-         * no hole is generated.  The partial disk is subdivided around the z axis
-         * into slices (like pizza slices), and also about the z axis into rings (as
-         * specified by slices and loops, respectively).
-         *
-         * With respect to orientation, the +z side of the partial disk is considered
-         * to be outside (see gluQuadricOrientation). This means that if the
-         * orientation is set to GLU_OUTSIDE, then any normals generated point along
-         * the +z axis. Otherwise, they point along the -z axis.
-         *
-         * If texturing is turned on (with gluQuadricTexture), texture coordinates are
-         * generated linearly such that where r=outerRadius, the value at (r, 0, 0) is
-         * (1, 0.5), at (0, r, 0) it is (0.5, 1), at (-r, 0, 0) it is (0, 0.5), and at
-         * (0, -r, 0) it is (0.5, 0).
-         */
-        public static native void gluPartialDisk(
-                ByteBuffer target,
-                double innerRadius,
-                double outerRadius,
-                int slices,
-                int loops,
-                double startAngle,
-                double sweepAngle
-        );
-
-        /**
-         * specifies the draw style for quadrics rendered with
-         * qobj.  The legal values are as follows:
-         *
-         * GLU.FILL:       Quadrics are rendered with polygon primitives. The polygons
-         *                 are drawn in a counterclockwise fashion with respect to
-         *                 their normals (as defined with glu.quadricOrientation).
-         *
-         * GLU.LINE:       Quadrics are rendered as a set of lines.
-         *
-         * GLU.SILHOUETTE: Quadrics are rendered as a set of lines, except that edges
-         * 		   separating coplanar faces will not be drawn.
-         *
-         * GLU.POINT:       Quadrics are rendered as a set of points.
-         */
-        public static native void gluQuadricDrawStyle(
-                ByteBuffer target,
-                int drawStyle
-        );
-        
-        /**
-         * specifies what kind	of normals are desired for quadrics
-         * rendered with	qobj.  The legal values	are as follows:
-         *
-         * GLU.NONE:     No normals are generated.
-         *
-         * GLU.FLAT:     One normal is generated for every facet of a quadric.
-         *
-         * GLU.SMOOTH:   One normal is generated for every vertex of a quadric.  This
-         *               is the default.
-         */
-        public static native void gluQuadricNormals(
-                ByteBuffer target,
-                int normals
-        );
-        
-        /**
-         * specifies what kind of orientation is desired for
-         * quadrics rendered with qobj.	The orientation	values are as follows:
-         *
-         * GLU.OUTSIDE:  Quadrics are drawn with normals pointing outward.
-         *
-         * GLU.INSIDE:   Normals point inward. The default is GLU.OUTSIDE.
-         *
-         * Note that the interpretation of outward and inward depends on the quadric
-         * being drawn.
-         */
-        public static native void gluQuadricOrientation(
-                ByteBuffer target,
-                int orientation
-        );
-        
-        /**
-         * specifies if texture coordinates should be generated for
-         * quadrics rendered with qobj. If the value of textureCoords is true,
-         * then texture coordinates are generated, and if textureCoords is false,
-         * they are not.. The default is false.
-         *
-         * The manner in which texture coordinates are generated depends upon the
-         * specific quadric rendered.
-         */
-        public static native void gluQuadricTexture(
-                ByteBuffer target,
-                boolean textureCoords
-        );
-        
-        /**
-         * draws a sphere of the given	radius centered	around the origin.
-         * The sphere is subdivided around the z axis into slices and along the z axis
-         * into stacks (similar to lines of longitude and latitude).
-         *
-         * If the orientation is set to GLU.OUTSIDE (with glu.quadricOrientation), then
-         * any normals generated point away from the center of the sphere. Otherwise,
-         * they point toward the center of the sphere.
-
-         * If texturing is turned on (with glu.quadricTexture), then texture
-         * coordinates are generated so that t ranges from 0.0 at z=-radius to 1.0 at
-         * z=radius (t increases linearly along longitudinal lines), and s ranges from
-         * 0.0 at the +y axis, to 0.25 at the +x axis, to 0.5 at the -y axis, to 0.75
-         * at the -x axis, and back to 1.0 at the +y axis.
-         */
-        public static native void gluSphere(
-                ByteBuffer target,
-                double radius,
-                int slices,
-                int stacks
-        );
-        
-/*        public static native void gluQuadricCallback(
-                ByteBuffer target,
-                int type,
-                String method
-        );
-*/
-        public static native void gluQuadricCallback(
-                ByteBuffer target,
-                int type,
-                Object obj,
-                String method
-        );
         
 }
