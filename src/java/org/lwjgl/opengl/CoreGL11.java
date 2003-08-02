0dea5d4a444df008d009596b6c3652dbf73d2251
Caspian Rychlik-Prince <cprince@puppygames.net>
2003-08-02 15:58:05 +0000

    Fixes

--- src/java/org/lwjgl/opengl/CoreGL11.java
+++ src/java/org/lwjgl/opengl/CoreGL11.java
@@ -493,6 +493,11 @@ public class CoreGL11 implements CoreGL11Constants {
 		nglTexGenfv(coord, pname, params, params.position());
 	}
 	private static native void nglTexGenfv(int coord, int pname, FloatBuffer params, int params_offset);
+	public static native void glTexGeni(int coord, int pname, int param);
+	public static void glTexGen(int coord, int pname, IntBuffer params) {
+		nglTexGenfi(coord, pname, params, params.position());
+	}
+	private static native void nglTexGenfi(int coord, int pname, IntBuffer params, int params_offset);
 	public static native void glTexEnvf(int target, int pname, float param);
 	public static native void glTexEnvi(int target, int pname, int param);
 	public static void glTexEnv(int target, int pname, FloatBuffer params) {
