711c157ce6c881679d63a4cc90b13e80a2f8eb00
Caspian Rychlik-Prince <cprince@puppygames.net>
2003-08-17 16:32:53 +0000

    Added button names and consistent query methods to the buttons, to map between names and indexes and back again

---
+++ src/java/org/lwjgl/input/Controller.java
@@ -0,0 +1,261 @@
+/* 
+ * Copyright (c) 2002 Lightweight Java Game Library Project
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ * 
+ * * Redistributions of source code must retain the above copyright 
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'Light Weight Java Game Library' nor the names of 
+ *   its contributors may be used to endorse or promote products derived 
+ *   from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.lwjgl.input;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.lwjgl.Sys;
+
+/**
+ * $Id$
+ * <br>
+ * A raw Controller interface. This can be used to poll the current state of a
+ * controllers buttons, and axis positions. The axis positions
+ * are returned as ints in the range -1000 to 1000.
+ * 
+ * No buffering is available.
+ *
+ * Currently n (native limits) buttons, the x, y, z axis (also rotational x,y ,
+ * z axis) is supported along with a POV (or HAT) and a slider
+ *
+ * The Controller implementation currently only supports the first attached device.
+ *
+ * @author Brian Matzon <brian@matzon.dk>
+ * @version $Revision$
+ */
+public class Controller {
+
+	static {
+		initialize();
+	}
+
+	/** Has the controller been created? */
+	private static boolean created;
+
+	/** The controller buttons status */
+	private static boolean[] buttons;
+
+	/** X position, range -1000 to 1000 */
+	public static int x = 0;
+
+	/** X rotational position, range -1000 to 1000 */
+	public static int rx = 0;
+
+	/** Y position, range -1000 to 1000 */
+	public static int y = 0;
+
+	/** Y rotational position, range -1000 to 1000 */
+	public static int ry = 0;
+
+	/** Z position, range -1000 to 1000 */
+	public static int z = 0;
+
+	/** Z rotational position, range -1000 to 1000 */
+	public static int rz = 0;
+
+	/** Position of Point of View from -1 to 27000 (360 degrees) */
+	public static int pov;
+
+	/** Slider position, range -1000 to 1000 */
+	public static int slider = 0;
+
+	/** Constant specifying centered POV */
+	public static final int POV_CENTER = -1;
+
+	/** Constant specifying nortward POV */
+	public static final int POV_NORTH = 0;
+
+	/** Constant specifying southward POV */
+	public static final int POV_SOUTH = 18000;
+
+	/** Constant specifying eastward POV */
+	public static final int POV_EAST = 27000;
+
+	/** Constant specifying westward POV */
+	public static final int POV_WEST = 9000;
+
+	/** Number of buttons on the controller */
+	public static int buttonCount = -1;
+
+	/** Does this controller support a x axis */
+	public static boolean hasXAxis = false;
+
+	/** Does this controller support a rotational x axis */
+	public static boolean hasRXAxis = false;
+
+	/** Does this controller support an y axis */
+	public static boolean hasYAxis = false;
+
+	/** Does this controller support a rotational y axis */
+	public static boolean hasRYAxis = false;
+
+	/** Does this controller support a z axis */
+	public static boolean hasZAxis = false;
+
+	/** Does this controller support a rotational z axis */
+	public static boolean hasRZAxis = false;
+
+	/** Does this controller support a Point-Of-View (hat) */
+	public static boolean hasPOV = false;
+
+	/** Does this controller support a slider */
+	public static boolean hasSlider = false;
+	
+	/** Button names. These are set upon create(), to names like BUTTON0, BUTTON1, etc. */
+	private static String[] buttonName;
+	private static final Map buttonMap = new HashMap(8);
+
+	/**
+	 * Controller cannot be constructed.
+	 */
+	private Controller() {
+	}
+
+	/**
+	 * Static initialization
+	 */
+	private static void initialize() {
+		System.loadLibrary(Sys.getLibraryName());
+		initIDs();
+		
+		// Assign names to all the buttons
+		buttonName = new String[8];
+		for (int i = 0; i < 8; i ++) {
+			buttonName[i] = "BUTTON" + i;
+			buttonMap.put(buttonName[i], new Integer(i));
+		}
+		
+	}
+
+	/**
+	 * "Create" the controller. The display must first have been created.
+	 * @throws Exception if the controller could not be created for any reason
+	 */
+	public static void create() throws Exception {
+		if (created) {
+			return;
+		}
+
+		if (!nCreate()) {
+			throw new Exception("The controller could not be created.");
+		}
+		
+		created = true;
+	}
+
+	/**
+	 * @return true if the controller has been created
+	 */
+	public static boolean isCreated() {
+		return created;
+	}
+
+	/**
+	 * "Destroy" the controller
+	 */
+	public static void destroy() {
+		if (!created) {
+			return;
+		}
+		
+		created = false;
+		nDestroy();
+	}
+
+	/**
+	 * Polls the controller.
+	 */
+	public static void poll() {
+		assert created : "The controller has not been created.";
+		nPoll();
+	}
+
+	/**
+	 * Tests if a particular button is down.
+	 * 
+	 * @param button The index of the button you wish to test (0..buttonCount-1)
+	 * @return true if the specified button is down
+	 * @see #buttonCount
+	 */
+	public static boolean isButtonDown(int button) {
+		assert created : "The controller has not been created.";
+		return buttons[button];
+	}
+	
+	/**
+	 * Gets a button's name
+	 * @param button The button
+	 * @return a String with the button's human readable name in it or null if the button is unnamed
+	 */
+	public static String getButtonName(int button) {
+		if (button < 0 || button >= buttonName.length)
+			return null;
+		else
+			return buttonName[button];
+	}
+	
+	/**
+	 * Get's a button's index. If the button is unrecognised then -1 is returned.
+	 * @param buttonName The button name
+	 */
+	public static int getButtonIndex(String buttonName) {
+		Integer ret  = (Integer) buttonMap.get(buttonName);
+		if (ret == null)
+			return -1;
+		else
+			return ret.intValue();
+	}
+
+	/**
+	 * Native method to poll the controller
+	 */
+	private static native void nPoll();
+
+	/**
+	 * Native method to create the controller
+	 * 
+	 * @return true if the controller was created
+	 */
+	private static native boolean nCreate();
+
+	/**
+	* Native method the destroy the controller
+	*/
+	private static native void nDestroy();
+
+	/**
+	 * Register fields with the native library
+	 */
+	private static native void initIDs();
+}
---
+++ src/java/org/lwjgl/input/Mouse.java
@@ -0,0 +1,301 @@
+/* 
+ * Copyright (c) 2002 Lightweight Java Game Library Project
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ * 
+ * * Redistributions of source code must retain the above copyright 
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'Light Weight Java Game Library' nor the names of 
+ *   its contributors may be used to endorse or promote products derived 
+ *   from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.lwjgl.input;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.lwjgl.*;
+
+/**
+ * $Id$
+ *
+ * A raw Mouse interface. This can be used to poll the current state of the
+ * mouse buttons, and determine the mouse movement delta since the last poll.
+ * 
+ * n buttons supported, n being a native limit. A scrolly wheel is also
+ * supported, if one such is available. All movement is reported as delta from
+ * last position.
+ * 
+ * @author cix_foo <cix_foo@users.sourceforge.net>
+ * @version $Revision$
+ */
+public class Mouse {
+	public final static int CURSOR_ONE_BIT_TRANSPARANCY = 1;
+	public final static int CURSOR_8_BIT_ALPHA = 2;
+	public final static int CURSOR_ANIMATION = 4;
+
+	/** Has the mouse been created? */
+	private static boolean created;
+
+	/** The mouse buttons status from the last poll */
+	private static boolean[] buttons;
+
+	/** Delta X */
+	public static int dx;
+
+	/** Delta Y */
+	public static int dy;
+
+	/** Delta Z */
+	public static int dwheel;
+
+	/** Number of buttons supported by the mouse */
+	public static int buttonCount = -1;
+
+	/** Does this mouse support a scroll wheel */
+	public static boolean hasWheel = false;
+
+	/** The current native cursor, if any */
+	private static Cursor currentCursor;
+
+	/** Button names. These are set upon create(), to names like BUTTON0, BUTTON1, etc. */
+	private static String[] buttonName;
+	private static final Map buttonMap = new HashMap(16);
+
+	static {
+		initialize();
+	}
+
+	/**
+	 * Mouse cannot be constructed.
+	 */
+	private Mouse() {
+	}
+
+	/**
+	 * Gets the currently bound native cursor, if any.
+	 *
+	 * @return the currently bound native cursor, if any.
+	 */
+	public static Cursor getNativeCursor() {
+		return currentCursor;
+	}
+
+	/**
+	 * Get the capabilities of the native cursor. Return a bit mask of the native cursor capabilities.
+	 * The CURSOR_ONE_BIT_TRANSPARANCY indicates support for cursors with one bit transparancy,
+	 * the CURSOR_8_BIT_ALPHA indicates support for 8 bit alpha and CURSOR_ANIMATION indicates
+	 * support for cursor animations.
+	 * 
+	 * @return A bit mask with native cursor capabilities.
+	 */
+	public static int getNativeCursorCaps() {
+		return nGetNativeCursorCaps();
+	}
+
+	/**
+	 * Native function to determine native cursor support
+	 */
+	private static native int nGetNativeCursorCaps();
+
+	/**
+	 * Binds a native cursor. If the cursor argument is null, the
+	 * native cursor is disabled, as if native cursors were not supported.
+	 * The Mouse must be created before a native cursor can be bound.
+	 *
+	 * NOTE: The native cursor is not constrained to the window, but
+	 * relative events will not be generated if the cursor is outside.
+	 * The initial position of the cursor is in the upper left corner of
+	 * the window, and the cursor will be moved to this origin when a
+	 * native cursor is set and the previous cursor is null.
+	 *
+	 * @param cursor the native cursor object to bind. May be null.
+	 * @return The previous Cursor object set, or null.
+         * @throws Exception if the cursor could not be set for any reason
+	 */
+	public static Cursor setNativeCursor(Cursor cursor) throws Exception {
+		assert created && ((getNativeCursorCaps() | CURSOR_ONE_BIT_TRANSPARANCY) != 0);
+		Cursor oldCursor = currentCursor;
+		currentCursor = cursor;
+		if (currentCursor != null) {
+			nSetNativeCursor(currentCursor.getHandle());
+		} else {
+			nSetNativeCursor(0);
+		}
+		return oldCursor;
+	}
+
+	/** Native method to set the native cursor */
+	private static native void nSetNativeCursor(int handle);
+
+	/**
+	 * Gets the minimum size of a native cursor. Can only be called if
+	 * The Mouse is created and cursor caps includes at least
+	 * CURSOR_ONE_BIT_TRANSPARANCY.
+	 *
+	 * @return the maximum size of a native cursor
+	 */
+	public static int getMinCursorSize() {
+		return nGetMinCursorSize();
+	}
+
+	/** Native method returning the minimum cursor size */
+	private static native int nGetMinCursorSize();
+
+	/**
+	 * Gets the maximum size of a native cursor. Can only be called if
+	 * The Mouse is created and cursor caps includes at least
+	 * CURSOR_ONE_BIT_TRANSPARANCY.
+	 *
+	 * @return the maximum size of a native cursor
+	 */
+	public static int getMaxCursorSize() {
+		return nGetMaxCursorSize();
+	}
+
+	/** Native method returning the maximum cursor size */
+	private static native int nGetMaxCursorSize();
+
+	/**
+	 * Static initialization
+	 */
+	private static void initialize() {
+		System.loadLibrary(Sys.getLibraryName());
+		initIDs();
+		
+		// Assign names to all the buttons
+		buttonName = new String[16];
+		for (int i = 0; i < 16; i ++) {
+			buttonName[i] = "BUTTON" + i;
+			buttonMap.put(buttonName[i], new Integer(i));
+		}
+	}
+
+	/**
+	 * Register fields with the native library
+	 */
+	private static native void initIDs();
+
+	/**
+	 * "Create" the mouse. The display must first have been created.
+	* 
+	 * @throws Exception if the mouse could not be created for any reason
+	 */
+	public static void create() throws Exception {
+		if (created)
+			return;
+		if (!nCreate())
+			throw new Exception("The mouse could not be created.");
+		created = true;
+		currentCursor = null;
+
+		//set mouse buttons
+		buttons = new boolean[buttonCount];
+		
+
+	}
+
+	/**
+	 * Native method to create the mouse.
+	 * 
+	 * @return true if the mouse was created
+	 */
+	private static native boolean nCreate();
+
+	/**
+	 * @return true if the mouse has been created
+	 */
+	public static boolean isCreated() {
+		return created;
+	}
+
+	/**
+	 * "Destroy" the mouse. Remember to reset the native cursor if
+	 * setNativeCursor() has been called with anything else than null.
+	 */
+	public static void destroy() {
+		assert currentCursor == null;
+		if (!created)
+			return;
+		created = false;
+		buttons = null;
+		currentCursor = null;
+
+		nDestroy();
+	}
+
+	/**
+	 * Native method the destroy the mouse
+	 */
+	private static native void nDestroy();
+
+	/**
+	 * Polls the mouse.
+	 */
+	public static void poll() {
+		assert created : "The mouse has not been created.";
+		nPoll();
+	}
+
+	/**
+	 * Native method to poll the mouse
+	 */
+	private static native void nPoll();
+
+	/**
+	 * See if a particular mouse button is down.
+	 * 
+	 * @param button The index of the button you wish to test (0..buttonCount-1)
+	 * @return true if the specified button is down
+	 */
+	public static boolean isButtonDown(int button) {
+		assert created : "The mouse has not been created.";
+		return buttons[button];
+	}
+	
+	/**
+	 * Gets a button's name
+	 * @param button The button
+	 * @return a String with the button's human readable name in it or null if the button is unnamed
+	 */
+	public static String getButtonName(int button) {
+		if (button < 0 || button >= buttonName.length)
+			return null;
+		else
+			return buttonName[button];
+	}
+	
+	/**
+	 * Get's a button's index. If the button is unrecognised then -1 is returned.
+	 * @param buttonName The button name
+	 */
+	public static int getButtonIndex(String buttonName) {
+		Integer ret  = (Integer) buttonMap.get(buttonName);
+		if (ret == null)
+			return -1;
+		else
+			return ret.intValue();
+	}
+
+}
