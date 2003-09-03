ad4203917b7ea78befa5819d180d315782ffc978
Elias Naur <elias.naur@gmail.com>
2003-09-03 15:04:14 +0000

    *** empty log message ***

--- src/native/autogen.sh
+++ src/native/autogen.sh
@@ -1,9 +1,5 @@
-#export WANT_AUTOCONF_2_5=1
-#WANT_AUTOMAKE_1_5=1
-
-#autoheader linux
 aclocal
 autoheader
-libtoolize --force
-autoconf
+libtoolize --automake
 automake --foreign --include-deps --add-missing --copy
+autoconf
