8419f4a8c1a83e1796eaae052a4bdf12315a092e
Ioannis Tsakpinis <iotsakp@gmail.com>
2004-01-30 20:39:36 +0000

    Extensions split out into separate classes

---
+++ src/native/common/nv/org_lwjgl_opengl_nv_NVPrimitiveRestart.cpp
@@ -0,0 +1,80 @@
+/*
+* Copyright (c) 2002 Lightweight Java Game Library Project
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are
+* met:
+*
+* * Redistributions of source code must retain the above copyright
+*   notice, this list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright
+*   notice, this list of conditions and the following disclaimer in the
+*   documentation and/or other materials provided with the distribution.
+*
+* * Neither the name of 'Light Weight Java Game Library' nor the names of
+*   its contributors may be used to endorse or promote products derived
+*   from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// ----------------------------------
+// IMPLEMENTATION OF NATIVE METHODS FOR CLASS: org.lwjgl.opengl.nv.NVPrimitiveRestart
+// ----------------------------------
+
+#include "org_lwjgl_opengl_nv_NVPrimitiveRestart.h"
+#include "extgl.h"
+#include "checkGLerror.h"
+
+typedef void (APIENTRY * glPrimitiveRestartNVPROC) (GLvoid);
+typedef void (APIENTRY * glPrimitiveRestartIndexNVPROC) (GLuint index);
+
+static glPrimitiveRestartNVPROC glPrimitiveRestartNV;
+static glPrimitiveRestartIndexNVPROC glPrimitiveRestartIndexNV;
+
+void extgl_InitNVPrimitiveRestart(JNIEnv *env, jobject ext_set)
+{
+	if (!extgl_Extensions.GL_NV_primitive_restart)
+		return;
+
+	glPrimitiveRestartNV = (glPrimitiveRestartNVPROC) extgl_GetProcAddress("glPrimitiveRestartNV");
+	glPrimitiveRestartIndexNV = (glPrimitiveRestartIndexNVPROC) extgl_GetProcAddress("glPrimitiveRestartIndexNV");
+
+	EXTGL_SANITY_CHECK(env, ext_set, GL_NV_primitive_restart)
+}
+
+/*
+ * Class:	org.lwjgl.opengl.nv.NVPrimitiveRestart
+ * Method:	glPrimitiveRestartNV
+ */
+JNIEXPORT void JNICALL Java_org_lwjgl_opengl_nv_NVPrimitiveRestart_glPrimitiveRestartNV
+	(JNIEnv * env, jclass clazz)
+{
+	CHECK_EXISTS(glPrimitiveRestartNV)
+	glPrimitiveRestartNV();
+	CHECK_GL_ERROR
+}
+
+/*
+ * Class:	org.lwjgl.opengl.nv.NVPrimitiveRestart
+ * Method:	glPrimitiveRestartIndexNV
+ */
+JNIEXPORT void JNICALL Java_org_lwjgl_opengl_nv_NVPrimitiveRestart_glPrimitiveRestartIndexNV
+	(JNIEnv * env, jclass clazz, jint index)
+{
+	CHECK_EXISTS(glPrimitiveRestartIndexNV)
+	glPrimitiveRestartIndexNV(index);
+	CHECK_GL_ERROR
+}
