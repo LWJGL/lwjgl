67b1ef58c0d4508cae2951aa4bf30a2c41214725
Brian Matzon <brian@matzon.dk>
2004-06-30 23:26:54 +0000

    include platform sdk home delete intermediate files

--- platform_build/win32_ms_cmdline/build-fmod.bat
+++ platform_build/win32_ms_cmdline/build-fmod.bat
@@ -1,12 +1,15 @@
 @echo off
 
-set JAVAHOME=C:\Java\j2sdk1.5.0
-set FMODHOME="C:\Program Files\fmodapi372win\api\inc"
-set COPTIONS=/I%FMODHOME% /I%JAVAHOME%\include /I%JAVAHOME%\include\win32 /I..\..\src\native\common /O2 /nologo /c
-set LINKEROPTS=/link
+set JAVAHOME=C:\Java\jdk1.5.0
+set FMODHOME="C:\Program Files\fmodapi373win\api\inc"
+set PLTSDKHOME="C:\Program Files\Microsoft SDK"
+set COPTIONS=/I%FMODHOME% /I%PLTSDKHOME%\include /I%JAVAHOME%\include /I%JAVAHOME%\include\win32 /I..\..\src\native\common /O2 /nologo /c
+set LINKEROPTS=/link /LIBPATH:%PLTSDKHOME%\Lib
 set LIBS=user32.lib Gdi32.lib Advapi32.lib
 
 for %%x in (..\..\src\native\common\fmod3\*.cpp) do cl %COPTIONS% %%x
 for %%x in (..\..\src\native\common\*common*.cpp) do cl %COPTIONS% %%x
 
 cl /LD /Felwjgl-fmod3.dll *.obj %LINKEROPTS% %LIBS%
+
+del *.obj *.exp *.lib
--- platform_build/win32_ms_cmdline/build.bat
+++ platform_build/win32_ms_cmdline/build.bat
@@ -1,11 +1,12 @@
 @echo off
 
-set JAVAHOME=C:\Java\j2sdk1.5.0
+set JAVAHOME=C:\Java\jdk1.5.0
 set ALHOME="c:\Program Files\OpenAL 1.0 Software Development Kit"
 set EAXHOME="c:\Program Files\Creative Labs\EAX 2.0 Extensions SDK"
-set DXHOME=C:\DXSDK
-set COPTIONS=/I%DXHOME%\Include /I%JAVAHOME%\include /I%JAVAHOME%\include\win32 /I%ALHOME%\Include /I%EAXHOME%\Include /I..\..\src\native\common /O2 /nologo /c
-set LINKEROPTS=/link /LIBPATH:%ALHOME%\libs /LIBPATH:%EAXHOME%\Libs /LIBPATH:%DXHOME%\Lib
+set DXHOME="C:\Program Files\DX90SDK"
+set PLTSDKHOME="C:\Program Files\Microsoft SDK"
+set COPTIONS=/I%DXHOME%\Include /I%PLTSDKHOME%\include /I%JAVAHOME%\include /I%JAVAHOME%\include\win32 /I%ALHOME%\Include /I%EAXHOME%\Include /I..\..\src\native\common /O2 /nologo /c
+set LINKEROPTS=/link /LIBPATH:%ALHOME%\libs /LIBPATH:%EAXHOME%\Libs /LIBPATH:%DXHOME%\Lib /LIBPATH:%PLTSDKHOME%\Lib
 set LIBS=dinput.lib dxguid.lib OpenAL32.lib alut.lib eaxguid.lib OpenGL32.Lib Version.lib user32.lib Gdi32.lib Advapi32.lib
 
 for %%x in (..\..\src\native\win32\*.cpp) do cl %COPTIONS% %%x
@@ -16,3 +17,5 @@ for %%x in (..\..\src\native\common\ext\*.cpp) do cl %COPTIONS% %%x
 for %%x in (..\..\src\native\common\ati\*.cpp) do cl %COPTIONS% %%x
 
 cl /LD /Felwjgl.dll *.obj %LINKEROPTS% %LIBS%
+
+del *.obj *.exp *.lib
