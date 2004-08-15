b9bf056c4c585f5f28edf467790176016149b82c
Ioannis Tsakpinis <iotsakp@gmail.com>
2004-08-15 23:03:54 +0000

    First GL 2.0 Extensions

--- src/java/org/lwjgl/opengl/GLContext.java
+++ src/java/org/lwjgl/opengl/GLContext.java
@@ -1,31 +1,31 @@
-/* 
+/*
  * Copyright (c) 2002-2004 LWJGL Project
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are 
+ * modification, are permitted provided that the following conditions are
  * met:
- * 
- * * Redistributions of source code must retain the above copyright 
+ *
+ * * Redistributions of source code must retain the above copyright
  *   notice, this list of conditions and the following disclaimer.
  *
  * * Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
  *
- * * Neither the name of 'LWJGL' nor the names of 
- *   its contributors may be used to endorse or promote products derived 
+ * * Neither the name of 'LWJGL' nor the names of
+ *   its contributors may be used to endorse or promote products derived
  *   from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
@@ -64,11 +64,12 @@ public final class GLContext {
 	/*
 	 * Available extensions
 	 */
-	public static boolean GL_ARB_imaging;
 	public static boolean GL_ARB_depth_texture;
+	public static boolean GL_ARB_draw_buffers;
 	public static boolean GL_ARB_fragment_program;
 	public static boolean GL_ARB_fragment_program_shadow;
 	public static boolean GL_ARB_fragment_shader;
+	public static boolean GL_ARB_imaging;
 	public static boolean GL_ARB_matrix_palette;
 	public static boolean GL_ARB_multisample;
 	public static boolean GL_ARB_multitexture;
@@ -88,6 +89,7 @@ public final class GLContext {
 	public static boolean GL_ARB_texture_env_dot3;
 	public static boolean GL_ARB_texture_mirrored_repeat;
 	public static boolean GL_ARB_texture_non_power_of_two;
+	public static boolean GL_ARB_texture_rectangle;
 	public static boolean GL_ARB_transpose_matrix;
 	public static boolean GL_ARB_vertex_blend;
 	public static boolean GL_ARB_vertex_buffer_object;
@@ -244,7 +246,7 @@ public final class GLContext {
 			// Yes, so we don't need to do anything. Our caps and function pointers are still valid.
 			return;
 		}
-		
+
 		// Ok, now it's the current context.
 		if (gl_ref_count == 0) {
 			loadOpenGLLibrary();
@@ -283,7 +285,7 @@ public final class GLContext {
 				default:
 					break;
 			}
-				
+
 		}
 		addExtensionClass(exts, exts_names, "EXTTextureCompressionS3TC", "");
 		String extensions_string = GL11.glGetString(GL11.GL_EXTENSIONS);
@@ -390,7 +392,7 @@ public final class GLContext {
 	private static native void nLoadOpenGLLibrary() throws LWJGLException;
 
 	/**
-	 * The OpenGL library reference count is decremented, and if it 
+	 * The OpenGL library reference count is decremented, and if it
 	 * reaches 0, the library is unloaded.
 	 */
 	public static void unloadOpenGLLibrary() {
