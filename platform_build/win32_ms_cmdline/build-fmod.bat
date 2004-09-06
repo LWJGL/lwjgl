505062df9dd1167375946045e4b28696fd5d6a9d
Brian Matzon <brian@matzon.dk>
2004-09-06 16:41:45 +0000

    enable exception handling copy to libs

--- platform_build/win32_ms_cmdline/build-fmod.bat
+++ platform_build/win32_ms_cmdline/build-fmod.bat
@@ -3,7 +3,7 @@
 set JAVAHOME=C:\Java\jdk1.5.0
 set FMODHOME="C:\Program Files\fmodapi373win\api\inc"
 set PLTSDKHOME="C:\Program Files\Microsoft SDK"
-set COPTIONS=/I%FMODHOME% /I%PLTSDKHOME%\include /I%JAVAHOME%\include /I%JAVAHOME%\include\win32 /I..\..\src\native\common /O2 /nologo /c
+set COPTIONS=/I%FMODHOME% /I%PLTSDKHOME%\include /I%JAVAHOME%\include /I%JAVAHOME%\include\win32 /I..\..\src\native\common /O2 /nologo /c /EHsc
 set LINKEROPTS=/link /LIBPATH:%PLTSDKHOME%\Lib
 set LIBS=user32.lib Gdi32.lib Advapi32.lib
 
@@ -13,3 +13,5 @@ for %%x in (..\..\src\native\common\*common*.cpp) do cl %COPTIONS% %%x
 cl /LD /Felwjgl-fmod3.dll *.obj %LINKEROPTS% %LIBS%
 
 del *.obj *.exp *.lib
+
+copy lwjgl-fmod3.dll ..\..\libs\
