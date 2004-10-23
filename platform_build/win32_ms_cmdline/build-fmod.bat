c7862ba9e6907067a39b4369a676eba02f4c872b
Mark Bernard <captainjester@users.sourceforge.net>
2004-10-23 01:43:15 +0000

    Changed bat file to get variables from OS instead of setting them in the file.

--- platform_build/win32_ms_cmdline/build-fmod.bat
+++ platform_build/win32_ms_cmdline/build-fmod.bat
@@ -1,17 +1,63 @@
 @echo off
 
-set JAVAHOME=C:\Java\jdk1.5.0
-set FMODHOME="C:\Program Files\fmodapi373win\api\inc"
-set PLTSDKHOME="C:\Program Files\Microsoft SDK"
-set COPTIONS=-DWIN32 /I%FMODHOME% /I%PLTSDKHOME%\include /I%JAVAHOME%\include /I%JAVAHOME%\include\win32 /I..\..\src\native\common /O2 /nologo /c /EHsc
-set LINKEROPTS=/link /LIBPATH:%PLTSDKHOME%\Lib
+if "%JAVA_HOME%" == "" goto errorjavahome
+if "%PLTSDKHOME%" == "" goto errorpltsdkhome
+if "%CHOME%" == "" goto errorchome
+if "%FMODHOME%" == "" goto errorfmodhome
+set COPTIONS=-DWIN32 /I"%FMODHOME%\api\inc" /I"%PLTSDKHOME%\include" /I"%CHOME%\include" /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"..\..\src\native\common" /O2 /nologo /c /EHsc
+set LINKEROPTS=/link /LIBPATH:"%PLTSDKHOME%\Lib" /LIBPATH:"%CHOME%\Lib"
 set LIBS=user32.lib Gdi32.lib Advapi32.lib
 
-for %%x in (..\..\src\native\common\fmod3\*.cpp) do cl %COPTIONS% %%x
-for %%x in (..\..\src\native\common\*common*.cpp) do cl %COPTIONS% %%x
+for %%x in (..\..\src\native\common\fmod3\*.c) do cl %COPTIONS% %%x
+for %%x in (..\..\src\native\common\*common*.c) do cl %COPTIONS% %%x
 
 cl /LD /Felwjgl-fmod3.dll *.obj %LINKEROPTS% %LIBS%
 
 del *.obj *.exp *.lib
 
 copy lwjgl-fmod3.dll ..\..\libs\
+
+goto end
+
+:errorjavahome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo JAVA_HOME not set.
+echo.
+goto error
+
+:errorpltsdkhome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo PLTSDKHOME not set.
+echo.
+goto error
+
+:errorchome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo CHOME not set.
+echo.
+goto error
+
+:errorfmodhome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo FMODHOME not set.
+echo.
+
+:error
+echo The following environment variables are required to be set.
+echo JAVA_HOME  The root directory where Java is installed
+echo PLTSDKHOME  "   "       "        "  Platform SDK is installed
+echo CHOME       "   "       "        "  Visual C++ toolkit is installed
+echo FMODHOME    "   "       "        "  FMOD API is installed
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+
+:end
--- platform_build/win32_ms_cmdline/build.bat
+++ platform_build/win32_ms_cmdline/build.bat
@@ -1,12 +1,13 @@
 @echo off
 
-set JAVAHOME="C:\Program Files\Java\jdk1.5.0"
-set ALHOME="c:\Program Files\Creative Labs\OpenAL 1.0 SDK"
-set EAXHOME="c:\Program Files\Creative Labs\EAX 2.0 Extensions SDK"
-set DXHOME="C:\DXSDK"
-set PLTSDKHOME="C:\Program Files\Microsoft SDK"
-set COPTIONS=/I%DXHOME%\Include /I%PLTSDKHOME%\include /I%JAVAHOME%\include /I%JAVAHOME%\include\win32 /I%ALHOME%\Include /I%EAXHOME%\Include /I..\..\src\native\common /O2 /nologo /c
-set LINKEROPTS=/link /LIBPATH:%ALHOME%\libs /LIBPATH:%EAXHOME%\Libs /LIBPATH:%DXHOME%\Lib /LIBPATH:%PLTSDKHOME%\Lib
+if "%JAVA_HOME%" == "" goto errorjavahome
+if "%PLTSDKHOME%" == "" goto errorpltsdkhome
+if "%CHOME%" == "" goto errorchome
+if "%ALHOME%" == "" goto erroralhome
+if "%EAXHOME%" == "" goto erroreaxhome
+if "%DXHOME%" == "" goto errordxhome
+set COPTIONS=/I"%DXHOME%\include" /I"%CHOME%\include" /I"%PLTSDKHOME%\include" /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"%ALHOME%\include" /I"%EAXHOME%\include" /I"..\..\src\native\common" /O2 /nologo /c 
+set LINKEROPTS=/link /LIBPATH:"%ALHOME%\libs" /LIBPATH:"%EAXHOME%\Libs" /LIBPATH:"%DXHOME%\Lib" /LIBPATH:"%PLTSDKHOME%\Lib" /LIBPATH:"%CHOME%\Lib"
 set LIBS=dinput.lib dxguid.lib OpenAL32.lib alut.lib eaxguid.lib OpenGL32.Lib Version.lib user32.lib Gdi32.lib Advapi32.lib
 
 for %%x in (..\..\src\native\win32\*.cpp) do cl %COPTIONS% %%x
@@ -19,3 +20,76 @@ for %%x in (..\..\src\native\common\ati\*.c) do cl %COPTIONS% %%x
 cl /LD /Felwjgl.dll *.obj %LINKEROPTS% %LIBS%
 
 del *.obj *.exp *.lib
+
+goto end
+
+:errorjavahome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo JAVA_HOME not set.
+echo.
+goto error
+
+:errorpltsdkhome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo PLTSDKHOME not set.
+echo.
+goto error
+
+:errorchome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo CHOME not set.
+echo.
+goto error
+
+:erroralhome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo ALHOME not set.
+echo.
+goto error
+
+:errordevilhome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo DEVILHOME not set.
+echo.
+goto error
+
+:erroreaxhome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo EAXHOME not set.
+echo.
+goto error
+
+:errordxhome
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+echo DXHOME not set.
+echo.
+goto error
+
+:error
+echo The following environment variables are required to be set.
+echo JAVA_HOME  The root directory where Java is installed
+echo PLTSDKHOME  "   "       "        "  Platform SDK is installed
+echo CHOME       "   "       "        "  Visual C++ toolkit is installed
+echo ALHOME      "   "       "        "  OpenAl is installed
+echo DEVILHOME   "   "       "        "  DevIL is installed
+echo EAXHOME     "   "       "        "  EAX is installed
+echo DXHOME      "   "       "        "  DirectX is installed
+echo -------------------------
+echo --     ** ERROR **     --
+echo -------------------------
+
+:end
