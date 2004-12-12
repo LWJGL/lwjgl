9494a40140812c009b6536e5c1d80ab77cf66d6c
Brian Matzon <brian@matzon.dk>
2004-12-12 10:44:34 +0000

    using vsnprintf for printfDebugJava *fix*

--- src/native/common/common_tools.c
+++ src/native/common/common_tools.c
@@ -79,7 +79,11 @@ void printfDebugJava(JNIEnv *env, const char *format, ...) {
 	va_list ap;
 	va_start(ap, format);
 	if (isDebugEnabled()) {
+		#ifdef WIN32
 		_vsnprintf(buffer, BUFFER_SIZE, format, ap);
+		#else
+		vsnprintf(buffer, BUFFER_SIZE, format, ap);
+		#endif
 		buffer[BUFFER_SIZE - 1] = '\0';
 		str = (*env)->NewStringUTF(env, buffer);
 		org_lwjgl_Sys_class = (*env)->FindClass(env, "org/lwjgl/Sys");
