e2cf927f8ef564df8328bcc76ba5aa390e520eda
Brian Matzon <brian@matzon.dk>
2005-03-25 13:21:04 +0000

    remove devil dependencies

--- platform_build/win32_ms_cmdline/build-devil.bat
+++ platform_build/win32_ms_cmdline/build-devil.bat
@@ -3,10 +3,9 @@
 if "%JAVA_HOME%" == "" goto errorjavahome
 if "%PLTSDKHOME%" == "" goto errorpltsdkhome
 if "%CHOME%" == "" goto errorchome
-if "%DEVILHOME%" == "" goto errordevilhome
-set COPTIONS=/I"%DEVILHOME%\include" /I"%PLTSDKHOME%\include" /I"%CHOME%\include" /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"..\..\src\native\common" /Ox /Ob2 /Oi /Ot /Oy /FD /EHsc /MT /Gy /W0 /nologo /c /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_USRDLL" /D "LWJGL_EXPORTS" /D "_WINDLL"
-set LINKEROPTS=/link /LIBPATH:"%PLTSDKHOME%\Lib" /LIBPATH:"%CHOME%\Lib" /LIBPATH:"%DEVILHOME%\Lib" /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /MACHINE:X86 /NOLOGO /DLL
-set LIBS=user32.lib Gdi32.lib Advapi32.lib DevIL.lib ILU.lib ILUT.lib
+set COPTIONS=/I"%PLTSDKHOME%\include" /I"%CHOME%\include" /I"%JAVA_HOME%\include" /I"%JAVA_HOME%\include\win32" /I"..\..\src\native\common" /Ox /Ob2 /Oi /Ot /Oy /FD /EHsc /MT /Gy /W0 /nologo /c /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_USRDLL" /D "LWJGL_EXPORTS" /D "_WINDLL"
+set LINKEROPTS=/link /LIBPATH:"%PLTSDKHOME%\Lib" /LIBPATH:"%CHOME%\Lib" /SUBSYSTEM:WINDOWS /OPT:REF /OPT:ICF /MACHINE:X86 /NOLOGO /DLL
+set LIBS=user32.lib Gdi32.lib Advapi32.lib
 
 for %%x in (..\..\src\native\common\devil\*.c) do cl %COPTIONS% %%x
 for %%x in (..\..\src\native\common\*common*.c) do cl %COPTIONS% %%x
@@ -43,19 +42,11 @@ echo CHOME not set.
 echo.
 goto error
 
-:errordevilhome
-echo -------------------------
-echo --     ** ERROR **     --
-echo -------------------------
-echo DEVILHOME not set.
-echo.
-
 :error
 echo The following environment variables are required to be set.
 echo JAVA_HOME  The root directory where Java is installed
 echo PLTSDKHOME  "   "       "        "  Platform SDK is installed
 echo CHOME       "   "       "        "  Visual C++ toolkit is installed
-echo DEVILHOME   "   "       "        "  DevIL API is installed
 echo -------------------------
 echo --     ** ERROR **     --
 echo -------------------------
