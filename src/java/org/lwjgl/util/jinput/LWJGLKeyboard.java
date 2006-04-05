f16f0e26cc9bcae4584bb3c1bebf7720deebe43c
Elias Naur <elias.naur@gmail.com>
2006-04-05 11:27:46 +0000

    Added a JInput plugin to interface with the LWJGL mouse and keyboard from jinput (untested)

---
+++ src/java/org/lwjgl/util/jinput/LWJGLKeyboard.java
@@ -0,0 +1,115 @@
+/**
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. Redistributions in binary 
+ * form must reproduce the above copyright notice, this list of conditions and 
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 
+ * The name of the author may not be used to endorse or promote products derived
+ * from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+ */
+package org.lwjgl.util.jinput;
+
+import net.java.games.input.Keyboard;
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.Rumbler;
+import net.java.games.input.Event;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.io.IOException;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/**
+ * @author elias
+ */
+final class LWJGLKeyboard extends Keyboard {
+    protected LWJGLKeyboard() {
+        super("LWJGLKeyboard", createComponents(), new Controller[]{}, new Rumbler[]{});
+	}
+
+	private final static Component[] createComponents() {
+		List components = new ArrayList();
+		Field[] vkey_fields = org.lwjgl.input.Keyboard.class.getFields();
+		for (int i = 0; i < vkey_fields.length; i++) {
+			Field vkey_field = vkey_fields[i];
+			try {
+				if (Modifier.isStatic(vkey_field.getModifiers()) && vkey_field.getType() == int.class &&
+						vkey_field.getName().startsWith("KEY_")) {
+					int vkey_code = vkey_field.getInt(null);
+					Component.Identifier.Key key_id = KeyMap.map(vkey_code);
+					if (key_id != Component.Identifier.Key.UNKNOWN)
+						components.add(new Key(key_id, vkey_code));
+				}
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return (Component[])components.toArray(new Component[]{});
+	}
+
+	public final synchronized void pollDevice() throws IOException {
+		if (!org.lwjgl.input.Keyboard.isCreated())
+			return;
+		org.lwjgl.input.Keyboard.poll();
+		Component[] components = getComponents();
+		for (int i = 0; i < components.length; i++) {
+			Key key = (Key)components[i];
+			key.update();
+		}
+	}
+
+	protected final synchronized boolean getNextDeviceEvent(Event event) throws IOException {
+		if (!org.lwjgl.input.Keyboard.isCreated())
+			return false;
+		if (!org.lwjgl.input.Keyboard.next())
+			return false;
+		int lwjgl_key = org.lwjgl.input.Keyboard.getEventKey();
+		if (lwjgl_key == org.lwjgl.input.Keyboard.KEY_NONE)
+			return false;
+		Component.Identifier.Key key_id = KeyMap.map(lwjgl_key);
+		if (key_id == null)
+			return false;
+		Component key = getComponent(key_id);
+		if (key == null)
+			return false;
+		float value = org.lwjgl.input.Keyboard.getEventKeyState() ? 1 : 0;
+		event.set(key, value);
+		return true;
+	}
+
+
+	private final static class Key extends Keyboard.Key {
+		private final int lwjgl_key;
+		private float value;
+		
+		public Key(Component.Identifier.Key key_id, int lwjgl_key) {
+			super(key_id.getName(), key_id);
+			this.lwjgl_key = lwjgl_key;
+		}
+
+		public final void update() {
+			this.value = org.lwjgl.input.Keyboard.isKeyDown(lwjgl_key) ? 1 : 0;
+		}
+		
+		protected final float poll() {
+			return value;
+		}
+	}
+}
---
+++ src/java/org/lwjgl/util/jinput/LWJGLMouse.java
@@ -0,0 +1,173 @@
+/**
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. Redistributions in binary 
+ * form must reproduce the above copyright notice, this list of conditions and 
+ * the following disclaimer in the documentation and/or other materials provided
+ * with the distribution. 
+ * The name of the author may not be used to endorse or promote products derived
+ * from this software without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO 
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
+ */
+
+package org.lwjgl.util.jinput;
+
+import net.java.games.input.Mouse;
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.Rumbler;
+import net.java.games.input.Event;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.io.IOException;
+
+/**
+ * @author elias
+ */
+final class LWJGLMouse extends Mouse {
+	private final static int EVENT_X = 1;
+	private final static int EVENT_Y = 2;
+	private final static int EVENT_WHEEL = 3;
+	private final static int EVENT_BUTTON = 4;
+	private final static int EVENT_DONE = 5;
+
+	private int event_state = EVENT_DONE;
+	
+    protected LWJGLMouse() {
+        super("LWJGLMouse", createComponents(), new Controller[]{}, new Rumbler[]{});
+    }
+
+	private final static Component[] createComponents() {
+		return new Component[]{new Axis(Component.Identifier.Axis.X),
+			new Axis(Component.Identifier.Axis.Y),
+			new Axis(Component.Identifier.Axis.Z),
+			new Button(Component.Identifier.Button.LEFT),
+			new Button(Component.Identifier.Button.MIDDLE),
+			new Button(Component.Identifier.Button.RIGHT)};
+	}
+
+	public final synchronized void pollDevice() throws IOException {
+		if (!org.lwjgl.input.Mouse.isCreated())
+			return;
+		org.lwjgl.input.Mouse.poll();
+		for (int i = 0; i < 3; i++)
+			setButtonState(i);
+	}
+
+	private final Button map(int lwjgl_button) {
+		switch (lwjgl_button) {
+			case 0:
+				return (Button)getLeft();
+			case 1:
+				return (Button)getRight();
+			case 2:
+				return (Button)getMiddle();
+			default:
+				return null;
+		}
+	}
+
+	private final void setButtonState(int lwjgl_button) {
+		Button button = map(lwjgl_button);
+		if (button != null)
+			button.setValue(org.lwjgl.input.Mouse.isButtonDown(lwjgl_button) ? 1 : 0);
+	}
+
+	protected final synchronized boolean getNextDeviceEvent(Event event) throws IOException {
+		if (!org.lwjgl.input.Mouse.isCreated())
+			return false;
+		while (true) {
+			switch (event_state) {
+				case EVENT_X:
+					event_state = EVENT_Y;
+					int dx = org.lwjgl.input.Mouse.getEventDX();
+					if (dx != 0) {
+						event.set(getX(), dx);
+						return true;
+					}
+					break;
+				case EVENT_Y:
+					event_state = EVENT_WHEEL;
+					/* We must negate the y coord since lwjgl uses the
+					 * OpenGL coordinate system
+					 */
+					int dy = -org.lwjgl.input.Mouse.getEventDY();
+					if (dy != 0) {
+						event.set(getY(), dy);
+						return true;
+					}
+					break;
+				case EVENT_WHEEL:
+					event_state = EVENT_BUTTON;
+					int dwheel = org.lwjgl.input.Mouse.getEventDWheel();
+					if (dwheel != 0) {
+						event.set(getWheel(), dwheel);
+						return true;
+					}
+					break;
+				case EVENT_BUTTON:
+					event_state = EVENT_DONE;
+					int lwjgl_button = org.lwjgl.input.Mouse.getEventButton();
+					if (lwjgl_button != -1) {
+						Button button = map(lwjgl_button);
+						if (button != null) {
+							event.set(button, org.lwjgl.input.Mouse.getEventButtonState() ? 1f : 0f);
+							return true;
+						}
+					}
+					break;
+				case EVENT_DONE:
+					if (!org.lwjgl.input.Mouse.next())
+						return false;
+					event_state = EVENT_X;
+					break;
+				default:
+					break;
+			}
+		}
+	}
+
+	final static class Axis extends Mouse.Axis {
+		public Axis(Component.Identifier.Axis axis_id) {
+			super(axis_id.getName(), axis_id);
+		}
+
+		public final boolean isRelative() {
+			return true;
+		}
+
+		protected final float poll() throws IOException {
+			return 0;
+		}
+	}
+
+	final static class Button extends Mouse.Button {
+		private float value;
+		
+		public Button(Component.Identifier.Button button_id) {
+			super(button_id.getName(), button_id);
+		}
+
+		protected final void setValue(float value) {
+			this.value = value;
+		}
+
+		protected final float poll() throws IOException {
+			return value;
+		}
+	}
+}
