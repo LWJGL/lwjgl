d7e7b299c168d28b41486692f8d6c3ed3afd60d9
Brian Matzon <brian@matzon.dk>
2006-07-02 21:58:00 +0000

    beta2 tag

--- src/native/common/common_tools.c
+++ src/native/common/common_tools.c
@@ -48,7 +48,7 @@
 #include "org_lwjgl_DefaultSysImplementation.h"
 
 static bool debug = false;
-static const char* VERSION = "1.0beta";
+static const char* VERSION = "1.0beta2";
 static JavaVM *jvm;
 
 void initAttribList(attrib_list_t *list) {
