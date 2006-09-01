71f67b1859f49649a124080fcc6702369929503c
Ioannis Tsakpinis <iotsakp@gmail.com>
2006-09-01 20:15:05 +0000

    Added INVALID_FRAMEBUFFER_OPERATION_EXT in GLU.gluErrorString

--- src/java/org/lwjgl/opengl/glu/GLU.java
+++ src/java/org/lwjgl/opengl/glu/GLU.java
@@ -1,31 +1,31 @@
-/* 
+/*
  * Copyright (c) 2002-2004 LWJGL Project
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are 
+ * modification, are permitted provided that the following conditions are
  * met:
- * 
- * * Redistributions of source code must retain the above copyright 
+ *
+ * * Redistributions of source code must retain the above copyright
  *   notice, this list of conditions and the following disclaimer.
  *
  * * Redistributions in binary form must reproduce the above copyright
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
  *
- * * Neither the name of 'LWJGL' nor the names of 
- *   its contributors may be used to endorse or promote products derived 
+ * * Neither the name of 'LWJGL' nor the names of
+ *   its contributors may be used to endorse or promote products derived
  *   from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
@@ -35,6 +35,7 @@ import java.nio.ByteBuffer;
 
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
+import org.lwjgl.opengl.EXTFramebufferObject;
 
 /**
  * GLU.java
@@ -51,53 +52,53 @@ public class GLU {
 	public static final int GLU_INVALID_VALUE       = 100901;
 	public static final int GLU_OUT_OF_MEMORY       = 100902;
 	public static final int GLU_INCOMPATIBLE_GL_VERSION     = 100903;
-	
+
 	/* StringName */
 	public static final int GLU_VERSION             = 100800;
 	public static final int GLU_EXTENSIONS          = 100801;
-	
+
 	/* Boolean */
 	public static final boolean GLU_TRUE                = true;
 	public static final boolean GLU_FALSE               = false;
-	
-	
+
+
 	/****           Quadric constants               ****/
-	
+
 	/* QuadricNormal */
 	public static final int GLU_SMOOTH              = 100000;
 	public static final int GLU_FLAT                = 100001;
 	public static final int GLU_NONE                = 100002;
-	
+
 	/* QuadricDrawStyle */
 	public static final int GLU_POINT               = 100010;
 	public static final int GLU_LINE                = 100011;
 	public static final int GLU_FILL                = 100012;
 	public static final int GLU_SILHOUETTE          = 100013;
-	
+
 	/* QuadricOrientation */
 	public static final int GLU_OUTSIDE             = 100020;
 	public static final int GLU_INSIDE              = 100021;
-	
+
 	/* Callback types: */
 	/*      ERROR               = 100103 */
-	
-	
+
+
 	/****           Tesselation constants           ****/
-	
+
 	public static final double TESS_MAX_COORD              = 1.0e150;
-	
+
 	/* TessProperty */
 	public static final int GLU_TESS_WINDING_RULE           = 100140;
 	public static final int GLU_TESS_BOUNDARY_ONLY          = 100141;
 	public static final int GLU_TESS_TOLERANCE              = 100142;
-	
+
 	/* TessWinding */
 	public static final int GLU_TESS_WINDING_ODD            = 100130;
 	public static final int GLU_TESS_WINDING_NONZERO        = 100131;
 	public static final int GLU_TESS_WINDING_POSITIVE       = 100132;
 	public static final int GLU_TESS_WINDING_NEGATIVE       = 100133;
 	public static final int GLU_TESS_WINDING_ABS_GEQ_TWO    = 100134;
-	
+
 	/* TessCallback */
 	public static final int GLU_TESS_BEGIN          = 100100;  /* void (CALLBACK*)(GLenum    type)  */
 	public static final int GLU_TESS_VERTEX         = 100101;  /* void (CALLBACK*)(void      *data) */
@@ -108,12 +109,12 @@ public class GLU {
 	                                                            void      *data[4],
 	                                                            GLfloat   weight[4],
 	                                                            void      **dataOut)     */
-	public static final int GLU_TESS_BEGIN_DATA     = 100106;  /* void (CALLBACK*)(GLenum    type,  
+	public static final int GLU_TESS_BEGIN_DATA     = 100106;  /* void (CALLBACK*)(GLenum    type,
 	                                                            void      *polygon_data) */
-	public static final int GLU_TESS_VERTEX_DATA    = 100107;  /* void (CALLBACK*)(void      *data, 
+	public static final int GLU_TESS_VERTEX_DATA    = 100107;  /* void (CALLBACK*)(void      *data,
 	                                                            void      *polygon_data) */
 	public static final int GLU_TESS_END_DATA       = 100108;  /* void (CALLBACK*)(void      *polygon_data) */
-	public static final int GLU_TESS_ERROR_DATA     = 100109;  /* void (CALLBACK*)(GLenum    errno, 
+	public static final int GLU_TESS_ERROR_DATA     = 100109;  /* void (CALLBACK*)(GLenum    errno,
 	                                                            void      *polygon_data) */
 	public static final int GLU_TESS_EDGE_FLAG_DATA = 100110;  /* void (CALLBACK*)(GLboolean boundaryEdge,
 	                                                            void      *polygon_data) */
@@ -122,7 +123,7 @@ public class GLU {
 	                                                            GLfloat   weight[4],
 	                                                            void      **dataOut,
 	                                                            void      *polygon_data) */
-	
+
 	/* TessError */
 	public static final int GLU_TESS_ERROR1     = 100151;
 	public static final int GLU_TESS_ERROR2     = 100152;
@@ -132,16 +133,16 @@ public class GLU {
 	public static final int GLU_TESS_ERROR6     = 100156;
 	public static final int GLU_TESS_ERROR7     = 100157;
 	public static final int GLU_TESS_ERROR8     = 100158;
-	
+
 	public static final int GLU_TESS_MISSING_BEGIN_POLYGON  = GLU_TESS_ERROR1;
 	public static final int GLU_TESS_MISSING_BEGIN_CONTOUR  = GLU_TESS_ERROR2;
 	public static final int GLU_TESS_MISSING_END_POLYGON    = GLU_TESS_ERROR3;
 	public static final int GLU_TESS_MISSING_END_CONTOUR    = GLU_TESS_ERROR4;
 	public static final int GLU_TESS_COORD_TOO_LARGE        = GLU_TESS_ERROR5;
 	public static final int GLU_TESS_NEED_COMBINE_CALLBACK  = GLU_TESS_ERROR6;
-	
+
 	/****           NURBS constants                 ****/
-	
+
 	/* NurbsProperty */
 	public static final int GLU_AUTO_LOAD_MATRIX    = 100200;
 	public static final int GLU_CULLING             = 100201;
@@ -151,25 +152,25 @@ public class GLU {
 	public static final int GLU_SAMPLING_METHOD             = 100205;
 	public static final int GLU_U_STEP                      = 100206;
 	public static final int GLU_V_STEP                      = 100207;
-	
+
 	/* NurbsSampling */
 	public static final int GLU_PATH_LENGTH                 = 100215;
 	public static final int GLU_PARAMETRIC_ERROR            = 100216;
 	public static final int GLU_DOMAIN_DISTANCE             = 100217;
-	
-	
+
+
 	/* NurbsTrim */
 	public static final int GLU_MAP1_TRIM_2         = 100210;
 	public static final int GLU_MAP1_TRIM_3         = 100211;
-	
+
 	/* NurbsDisplay */
 	/*      FILL                = 100012 */
 	public static final int GLU_OUTLINE_POLYGON     = 100240;
 	public static final int GLU_OUTLINE_PATCH       = 100241;
-	
+
 	/* NurbsCallback */
 	/*      ERROR               = 100103 */
-	
+
 	/* NurbsErrors */
 	public static final int GLU_NURBS_ERROR1        = 100251;
 	public static final int GLU_NURBS_ERROR2        = 100252;
@@ -208,14 +209,14 @@ public class GLU {
 	public static final int GLU_NURBS_ERROR35       = 100285;
 	public static final int GLU_NURBS_ERROR36       = 100286;
 	public static final int GLU_NURBS_ERROR37       = 100287;
-	
+
 	/* Contours types -- obsolete! */
 	public static final int GLU_CW          = 100120;
 	public static final int GLU_CCW         = 100121;
 	public static final int GLU_INTERIOR    = 100122;
 	public static final int GLU_EXTERIOR    = 100123;
 	public static final int GLU_UNKNOWN     = 100124;
-	
+
 	/* Names without "TESS_" prefix */
 	public static final int GLU_BEGIN       = GLU_TESS_BEGIN;
 	public static final int GLU_VERTEX      = GLU_TESS_VERTEX;
@@ -426,6 +427,8 @@ public class GLU {
 				return "Out of memory";
 			case GL12.GL_TABLE_TOO_LARGE:
 				return "Table too large";
+			case EXTFramebufferObject.GL_INVALID_FRAMEBUFFER_OPERATION_EXT:
+				return "Invalid framebuffer operation";
 			case GLU.GLU_INVALID_ENUM:
 				return "Invalid enum (glu)";
 			case GLU.GLU_INVALID_VALUE:
