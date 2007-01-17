d3f6588580249ce8c020730afffa5ea541aed10c
Elias Naur <elias.naur@gmail.com>
2007-01-17 08:01:49 +0000

    Auto generated files update

--- src/native/generated/org_lwjgl_opengl_NVFramebufferMultisampleCoverage.c
+++ src/native/generated/org_lwjgl_opengl_NVFramebufferMultisampleCoverage.c
@@ -3,10 +3,10 @@
 #include <jni.h>
 #include "extgl.h"
 
-typedef void (APIENTRY *glRenderbufferStorageMultsampleCoverageNVPROC) (GLenum target, GLsizei coverageSamples, GLsizei colorSamples, GLenum internalformat, GLsizei width, GLsizei height);
+typedef void (APIENTRY *glRenderbufferStorageMultisampleCoverageNVPROC) (GLenum target, GLsizei coverageSamples, GLsizei colorSamples, GLenum internalformat, GLsizei width, GLsizei height);
 
-JNIEXPORT void JNICALL Java_org_lwjgl_opengl_NVFramebufferMultisampleCoverage_nglRenderbufferStorageMultsampleCoverageNV(JNIEnv *env, jclass clazz, jint target, jint coverageSamples, jint colorSamples, jint internalformat, jint width, jint height, jlong function_pointer) {
-	glRenderbufferStorageMultsampleCoverageNVPROC glRenderbufferStorageMultsampleCoverageNV = (glRenderbufferStorageMultsampleCoverageNVPROC)((intptr_t)function_pointer);
-	glRenderbufferStorageMultsampleCoverageNV(target, coverageSamples, colorSamples, internalformat, width, height);
+JNIEXPORT void JNICALL Java_org_lwjgl_opengl_NVFramebufferMultisampleCoverage_nglRenderbufferStorageMultisampleCoverageNV(JNIEnv *env, jclass clazz, jint target, jint coverageSamples, jint colorSamples, jint internalformat, jint width, jint height, jlong function_pointer) {
+	glRenderbufferStorageMultisampleCoverageNVPROC glRenderbufferStorageMultisampleCoverageNV = (glRenderbufferStorageMultisampleCoverageNVPROC)((intptr_t)function_pointer);
+	glRenderbufferStorageMultisampleCoverageNV(target, coverageSamples, colorSamples, internalformat, width, height);
 }
 
