9ebea51be1af7783a6c914f998caa0c369e7a0a4
Brian Matzon <brian@matzon.dk>
2007-10-28 21:42:43 +0000

    updating version string to 1.1.3

--- src/java/org/lwjgl/devil/ILNative.java
+++ src/java/org/lwjgl/devil/ILNative.java
@@ -53,7 +53,7 @@ class ILNative {
 	private static String JNI_LIBRARY_NAME = "lwjgl-devil";
 
 	/** Version of IL */
-	static final String VERSION = "1.1.2";
+	static final String VERSION = "1.1.3";
 	
 	/** Current version of the JNI library */
 	static final int JNI_VERSION = 2;	
