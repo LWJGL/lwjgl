c456d9c3238b9d7a9e444f4c93f75214ed104fd7
Elias Naur <elias.naur@gmail.com>
2008-04-07 16:37:39 +0000

    Really fixed MouseTest

--- src/java/org/lwjgl/test/input/MouseTest.java
+++ src/java/org/lwjgl/test/input/MouseTest.java
@@ -36,7 +36,7 @@ import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.glu.GLU;
+import org.lwjgl.util.glu.GLU;
 import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
 
