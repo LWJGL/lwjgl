c8d4eb6a8623f06fc6bae594446744fa3c22465c
Brian Matzon <brian@matzon.dk>
2008-04-11 12:21:25 +0000

    alpha 4

--- src/java/org/lwjgl/Sys.java
+++ src/java/org/lwjgl/Sys.java
@@ -54,7 +54,7 @@ public final class Sys {
 	private static final String JNI_LIBRARY_NAME = "lwjgl";
 
 	/** Current version of library */
-	private static final String VERSION = "2.0a3";
+	private static final String VERSION = "2.0a4";
 
 	/** Current version of the JNI library */
 	static final int JNI_VERSION = 15;
