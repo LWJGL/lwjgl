5c7c1eeacc74248705176f9d899f2d7fc0aef0d2
Brian Matzon <brian@matzon.dk>
2010-02-03 21:03:32 +0000

    prepare for 2.3

--- src/java/org/lwjgl/Sys.java
+++ src/java/org/lwjgl/Sys.java
@@ -54,7 +54,7 @@ public final class Sys {
 	private static final String JNI_LIBRARY_NAME = "lwjgl";
 
 	/** Current version of library */
-	private static final String VERSION = "2.2.2";
+	private static final String VERSION = "2.3";
 
 	/** The implementation instance to delegate platform specific behavior to */
 	private final static SysImplementation implementation;
