38a60ec53332a913e7b58214e9ff3aef748ecccb
kappaOne <one.kappa@googlemail.com>
2013-11-14 15:22:39 +0000

    Enable High DPI mode by default (where available) when using fullscreen mode on OS X as no special changes/scaling is required by applications.

--- src/java/org/lwjgl/opengl/MacOSXDisplay.java
+++ src/java/org/lwjgl/opengl/MacOSXDisplay.java
@@ -141,7 +141,7 @@ final class MacOSXDisplay implements DisplayImplementation {
 		
 		// OS X high DPI mode is only available on OS X 10.7+
 		enableHighDPI = LWJGLUtil.isMacOSXEqualsOrBetterThan(10, 7) && parent == null &&
-											Display.getPrivilegedBoolean("org.lwjgl.opengl.Display.enableHighDPI");
+											(Display.getPrivilegedBoolean("org.lwjgl.opengl.Display.enableHighDPI") || fullscreen);
 		
 		if (parented) this.canvas = parent;
 		else this.canvas = null;
