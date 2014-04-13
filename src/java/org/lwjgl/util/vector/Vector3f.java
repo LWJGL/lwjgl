cef2fda6c658802464bf99b56bc70203cd8a3440
IGotWood <jeffwebster.cpp@gmail.com>
2014-04-13 18:46:29 -0400

    Added some shorthand methods to vector3f like Vector3f.one

--- src/java/org/lwjgl/util/vector/Vector3f.java
+++ src/java/org/lwjgl/util/vector/Vector3f.java
@@ -121,6 +121,38 @@ public class Vector3f extends Vector implements Serializable, ReadableVector3f, 
 		return this;
 	}
 
+	public static Vector3f zero() {
+		return new Vector3f(0, 0, 0);
+	}
+	
+	public static Vector3f one() {
+		return new Vector3f(1, 1, 1);
+	}
+	
+	public static Vector3f up() {
+		return new Vector3f(0, 1, 0);
+	}
+	
+	public static Vector3f down() {
+		return new Vector3f(0, -1, 0);
+	}
+	
+	public static Vector3f right() {
+		return new Vector3f(1, 0, 0);
+	}
+	
+	public static Vector3f left() {
+		return new Vector3f(-1, 0, 0);
+	}
+	
+	public static Vector3f forward() {
+		return new Vector3f(0, 0, 1);
+	}
+	
+	public static Vector3f back() {
+		return new Vector3f(0, 0, -1);
+	}
+
 	/**
 	 * Add a vector to another vector and place the result in a destination
 	 * vector.
