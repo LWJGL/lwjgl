6376c705578b3c44a54d7d12a824bf0c0b242556
b23prodtm <b23prodtm@users.sourceforge.net>
2015-04-18 17:18:37 +0200

    WindowsKeyboard.releaseAll() needs current event time

--- src/java/org/lwjgl/opengl/WindowsDisplay.java
+++ src/java/org/lwjgl/opengl/WindowsDisplay.java
@@ -381,7 +381,7 @@ final class WindowsDisplay implements DisplayImplementation {
 				updateClipping();
 		} else {
 			if ( keyboard != null )
-				keyboard.releaseAll(millis);
+				keyboard.releaseAll(System.currentTimeMillis());
 			if ( Display.isFullscreen() ) {
 				showWindow(getHwnd(), SW_SHOWMINNOACTIVE);
 				resetDisplayMode();
