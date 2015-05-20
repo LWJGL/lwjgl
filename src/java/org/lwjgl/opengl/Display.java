a5c4462a3ed0ff39bc86b346a4de91d80c85406f
Ioannis Tsakpinis <iotsakp@gmail.com>
2015-05-20 15:20:12 +0300

    Ignore setDisplayMode when the Display parent is set. Fix #98

--- src/java/org/lwjgl/opengl/Display.java
+++ src/java/org/lwjgl/opengl/Display.java
@@ -239,21 +239,21 @@ public final class Display {
 				throw new NullPointerException("mode must be non-null");
 			boolean was_fullscreen = isFullscreen();
 			current_mode = mode;
-			if ( isCreated() ) {
-				destroyWindow();
-				// If mode is not fullscreen capable, make sure we are in windowed mode
-				try {
-					if ( was_fullscreen && !isFullscreen() )
-						display_impl.resetDisplayMode();
-                    else if ( isFullscreen() )
-						switchDisplayMode();
-					createWindow();
-					makeCurrentAndSetSwapInterval();
-				} catch (LWJGLException e) {
-					drawable.destroy();
+			if ( !isCreated() || parent != null )
+				return;
+			destroyWindow();
+			// If mode is not fullscreen capable, make sure we are in windowed mode
+			try {
+				if ( was_fullscreen && !isFullscreen() )
 					display_impl.resetDisplayMode();
-					throw e;
-				}
+                else if ( isFullscreen() )
+					switchDisplayMode();
+				createWindow();
+				makeCurrentAndSetSwapInterval();
+			} catch (LWJGLException e) {
+				drawable.destroy();
+				display_impl.resetDisplayMode();
+				throw e;
 			}
 		}
 	}
